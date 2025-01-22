accca58177f5477cdffc23d908befb1464f383e1
root <root@8083b509234d>
2025-01-22 21:52:22 +0530

    Pushed Successfully

--- conftest.py
+++ conftest.py
@@ -1,19 +1,19 @@
-# import pytest
-# from selenium import webdriver
-# from pages.home_page import Edit
-# from selenium.webdriver.common.by import By
+import pytest
+from selenium import webdriver
+from pages.home_page import Edit
+from selenium.webdriver.common.by import By
 
-# import pytest
-# from selenium import webdriver
-# from time import sleep
+import pytest
+from selenium import webdriver
+from time import sleep
 
-# @pytest.fixture(scope="function")
-# def setup(request):
-#     driver = webdriver.Chrome()
-#     driver.implicitly_wait(10)
-#     url = "https://insight.netgear.com/#/landingPage"
-#     driver.get(url)
-#     driver.maximize_window()
-#     request.cls.driver = driver
-#     yield
-#     driver.quit()
+@pytest.fixture(scope="function")
+def setup(request):
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    url = "https://insight.netgear.com/#/landingPage"
+    driver.get(url)
+    driver.maximize_window()
+    request.cls.driver = driver
+    yield
+    driver.quit()
